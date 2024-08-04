4fe6a37aa7df6c42dff280ef97c593f895b11821
Y-Elsayed <108357222+Y-Elsayed@users.noreply.github.com>
2024-08-04 20:34:36 +0300

    Added validation to ensure user possesses the specified card.

--- MoneyTransfer/src/main/java/com/BM/MoneyTransfer/service/TransactionServiceImpl.java
+++ MoneyTransfer/src/main/java/com/BM/MoneyTransfer/service/TransactionServiceImpl.java
@@ -2,17 +2,22 @@ package com.BM.MoneyTransfer.service;
 
 import com.BM.MoneyTransfer.dao.CardDao;
 import com.BM.MoneyTransfer.dao.TransactionDao;
+import com.BM.MoneyTransfer.dao.UserDao;
 import com.BM.MoneyTransfer.dto.enums.Status;
 import com.BM.MoneyTransfer.entity.Card;
 import com.BM.MoneyTransfer.entity.Transaction;
+import com.BM.MoneyTransfer.entity.User;
 import com.BM.MoneyTransfer.exception.custom.InsufficientFundsException;
 import com.BM.MoneyTransfer.exception.custom.RecipientNotFoundException;
+import com.BM.MoneyTransfer.exception.custom.SenderNotFoundException;
+import com.BM.MoneyTransfer.exception.custom.UserCardNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -21,8 +26,20 @@ import java.util.Optional;
 public class TransactionServiceImpl implements TransactionService {
 
     private final CardDao cardDao;
-    TransactionDao transactionDao;
-    CardService cardService;
+    private final UserDao userDao;
+    private final TransactionDao transactionDao;
+    private final CardService cardService;
+
+
+    @Override
+    public Optional<Transaction> findById(Long id) {
+        return this.transactionDao.findById(id);
+    }
+
+    @Override
+    public Page<Transaction> findByEmail(String email, Pageable pageable) {
+        return this.transactionDao.findByEmail(email, pageable);
+    }
 
     @Override
     @Transactional(isolation = Isolation.SERIALIZABLE)
@@ -48,17 +65,8 @@ public class TransactionServiceImpl implements TransactionService {
         return transaction;
     }
 
-    @Override
-    public Optional<Transaction> findById(Long id) {
-        return this.transactionDao.findById(id);
-    }
 
-    @Override
-    public Page<Transaction> findByEmail(String email, Pageable pageable) {
-        return this.transactionDao.findByEmail(email, pageable);
-    }
-
-    protected void validateTransaction(Transaction transaction) throws RecipientNotFoundException, InsufficientFundsException {
+    private void validateTransaction(Transaction transaction) throws RecipientNotFoundException, InsufficientFundsException {
 
         Card recipientCard = cardService.getCard(transaction.getRecipientCardNumber());
         if (recipientCard == null) {
@@ -70,12 +78,27 @@ public class TransactionServiceImpl implements TransactionService {
         }
     }
 
-    protected void performTransaction(Transaction transaction) {
-        Card senderCard = cardService.getCard(transaction.getSenderCardNumber());
+    private void performTransaction(Transaction transaction) throws SenderNotFoundException,UserCardNotFoundException{
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<User> userOptional = userDao.getUserWithCards(userName);
+        // Sanity Check
+        if (userOptional.isEmpty()) {
+            throw new SenderNotFoundException("Sender not found");
+        }
+
+        User user = userOptional.get();
+        Card senderCard = findUserCard(user, transaction.getSenderCardNumber());
+
         Card recipientCard = cardService.getCard(transaction.getRecipientCardNumber());
 
         cardService.updateCardBalance(senderCard.getCardNumber(), senderCard.getBalance() - transaction.getAmount());
         cardService.updateCardBalance(recipientCard.getCardNumber(), recipientCard.getBalance() + transaction.getAmount());
     }
-
+    private Card findUserCard(User user, String cardNumber) throws UserCardNotFoundException {
+        return user.getCards().stream()
+                .filter(card -> card.getCardNumber().equals(cardNumber))
+                .findFirst()
+                .orElseThrow(() -> new UserCardNotFoundException("The user does not have the specified card"));
+    }
 }
+
